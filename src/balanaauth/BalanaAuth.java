9f5be138ed394aefdc19fd8a7a4b50598f8a0ae0
ledannyang <danny27995@gmail.com>
2019-05-28 17:24:30 +0200

    refactored

--- src/balanaauth/BalanaAuth.java
+++ src/balanaauth/BalanaAuth.java
@@ -1,9 +1,7 @@
 package src.balanaauth;
 
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
 import java.util.Scanner;
 import org.wso2.balana.Balana;
 import org.wso2.balana.Indenter;
@@ -14,39 +12,29 @@ import org.wso2.balana.finder.impl.FileBasedPolicyFinderModule;
 
 public class BalanaAuth {
     private static final String pathToRequestFile = "src/resources/request/XACMLRequest";
-    private static final String pathToSaveFile = "src/resources/outputs/XACML";
-    
     private static Balana balana;
 
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Seleccione el Request deseado [1,5]");
         int requestNumber = scanner.nextInt();
-        
-        initBalana();
-        PDP pdp = getPDPNewInstance();        
-        
-        String requestFile = pathToRequestFile + requestNumber + ".xml";
-        String response = pdp.evaluate(Utils.getXMLFromFilePath(requestFile));
-        ResponseCtx responseCtx = ResponseCtx.getInstance(Utils.getXacmlResponse(response));
 
-        String pathToSave = pathToSaveFile + "PolicyRequest" + requestNumber;
-        OutputStream outputStream = new FileOutputStream(pathToSave);
-        Utils.printResult(outputStream, new Indenter(), responseCtx.getResults());
-    }
-
-    private static PDP getPDPNewInstance(){
-        PDPConfig pdpConfig = balana.getPdpConfig();
-        return new PDP(new PDPConfig(pdpConfig.getAttributeFinder(), pdpConfig.getPolicyFinder(), null, true));
-    }
-    
-    private static void initBalana(){
         try {
             String policyLocation = (new File(".")).getCanonicalPath() + File.separator + "src" + File.separator + "resources" + File.separator + "policy";
             System.setProperty(FileBasedPolicyFinderModule.POLICY_DIR_PROPERTY, policyLocation);
         } catch (IOException e) {
-            System.err.println("Can not locate policy repository");
+            System.err.println("No existe la politica");
         }
+
         balana = Balana.getInstance();
+
+        PDPConfig pdpConfig = balana.getPdpConfig();
+        PDP pdp = new PDP(new PDPConfig(pdpConfig.getAttributeFinder(), pdpConfig.getPolicyFinder(), null, true));
+
+        String requestFile = pathToRequestFile + requestNumber + ".xml";
+        String response = pdp.evaluate(Utils.getXMLFromFilePath(requestFile));
+
+        ResponseCtx responseCtx = ResponseCtx.getInstance(Utils.getXacmlResponse(response));
+        Utils.printResult(System.out, new Indenter(), responseCtx.getResults());
     }
 }
